c1e52a6c1aa62e83a82df1389e7ac912e6fd69bd
Yujian Zhang <yujian.zhang@gmail.com>
2014-04-30 00:32:04 +0800

    Add initial version of project files.

---
+++ src/clojure/sanguo/core/main.clj
@@ -0,0 +1,13 @@
+(ns sanguo.core.main
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]))
+
+(defactivity sanguo.core.MainActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "Hello from Clojure!"}]])))))
