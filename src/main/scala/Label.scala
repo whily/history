4fe1e7894e843a025f687fd99d57f547de99cdf4
Yujian Zhang <yujian.zhang@gmail.com>
2014-08-24 05:25:58 +0800

    Add skeleton classes for labels.

---
+++ src/main/scala/Label.scala
@@ -0,0 +1,44 @@
+/**
+ * Labels to draw on map.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2014 Yujian Zhang
+ */
+
+package net.whily.android.history
+
+import android.graphics.{Canvas, Paint}
+
+/** Labels to draw on map. 
+  * @param text label text
+  * @param size label size in dp
+  */
+abstract class Label(text: String, size: Int) {
+  def draw(canvas: Canvas, paint: Paint)
+}
+
+class Point(lon: Double, lat: Double)
+class Polyline(points: Array[Point])
+class Polygon(points: Array[Point])
+
+class PointLabel(text: String, size: Int, point: Point) extends
+    Label(text, size) {
+  def draw(canvas: Canvas, paint: Paint) {
+  }
+}
+
+class LineLabel(text: String, size: Int, line: Polyline) extends
+    Label(text, size) {
+  def draw(canvas: Canvas, paint: Paint) {
+  }
+}
+
+class AreaLabel(text: String, size: Int, area: Polygon) extends
+    Label(text, size) {
+  def draw(canvas: Canvas, paint: Paint) {
+  }
+}
