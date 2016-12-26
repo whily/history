b7910f111b191c923b832121bdba563651517e14
Yujian Zhang <yujian.zhang@gmail.com>
2016-12-26 16:44:05 +0800

    Cleanup.

--- src/main/scala/MapActivity.scala
+++ src/main/scala/MapActivity.scala
@@ -47,28 +47,3 @@ class MapActivity extends Activity {
     Util.requestImmersiveMode(this)
   }
 }
-
-/*
-HomeFragment.java
-package info.androidhive.slidingmenu;
-
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-public class HomeFragment extends Fragment {
-
-    public HomeFragment(){}
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        return rootView;
-    }
-}
- */
