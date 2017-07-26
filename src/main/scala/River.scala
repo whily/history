01b0f7c29c6b78a42f4d53b24d899d6b7232f4e3
Yujian Zhang <yujian.zhang@gmail.com>
2017-07-26 22:29:47 +0800

    Add River.scala.

---
+++ src/main/scala/River.scala
@@ -0,0 +1,29 @@
+/**
+ * River.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2017 Yujian Zhang
+ */
+
+package net.whily.android.history
+
+import net.whily.scasci.geo.Point
+
+class River(coordinates: Array[Double]) {
+  // TODO: if performance is an issue, one approach is to partition pointes into
+  // a recurives binary trees. The leaf point could be one segment contain x points, with
+  // x configurable. Each segment stores the information of the bounding rectangle, while
+  // the parent node contains the bounding rectangle of all the children. When drawing the river,
+  // a recursive method can be called from the root node.
+  assert(coordinates.length % 2 == 0)
+  private val nPoints = coordinates.length / 2
+  val points = new Array[Point](nPoints)
+  for (i <- 0 until nPoints) {
+    points(i) = Point(coordinates(2 * i), coordinates(2 * i + 1))
+  }
+}
+
