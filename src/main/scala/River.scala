cb9f34c31897313610b42a8b79063748dcb6608f
Yujian Zhang <yujian.zhang@gmail.com>
2017-12-25 14:48:56 +0800

    Add river width.

--- src/main/scala/River.scala
+++ src/main/scala/River.scala
@@ -3,7 +3,7 @@
  *
  * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
  *
- * License: 
+ * License:
  *   GNU General Public License v2
  *   http://www.gnu.org/licenses/gpl-2.0.html
  * Copyright (C) 2017 Yujian Zhang
@@ -13,17 +13,18 @@ package net.whily.android.history
 
 import net.whily.scasci.geo.Point
 
-class River(coordinates: Array[Double]) {
+/** Width represents how wide the river is, in range [1, 10] with 10 the widest.
+  * Only approximation to draw river. */
+class River(val width: Int, coordinates: Array[Double]) {
   // TODO: if performance is an issue, one approach is to partition pointes into
   // a recurives binary trees. The leaf point could be one segment contain x points, with
   // x configurable. Each segment stores the information of the bounding rectangle, while
   // the parent node contains the bounding rectangle of all the children. When drawing the river,
   // a recursive method can be called from the root node.
-  assert(coordinates.length % 2 == 0)
+  assert((1 <= width) && (width <= 10) && (coordinates.length % 2 == 0))
   private val nPoints = coordinates.length / 2
   val points = new Array[Point](nPoints)
   for (i <- 0 until nPoints) {
     points(i) = Point(coordinates(2 * i), coordinates(2 * i + 1))
   }
 }
-
